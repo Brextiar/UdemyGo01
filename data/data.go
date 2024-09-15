16e3468b8af284223d21fccca3c8d0e0221c2838
Antoine Ferron <antoine.ferron6@gmail.com>
2024-09-15 14:02:24 +0200

    First commit

---
+++ data/data.go
@@ -0,0 +1,6 @@
+package data
+
+var Name = "bob"
+var Age = 10
+
+var Password = "1234" // variable privée
---
+++ input/keyboard.go
@@ -0,0 +1,7 @@
+package input
+
+import "fmt"
+
+func Keyboard() {
+	fmt.Println("Keyboard function from the input package")
+}
