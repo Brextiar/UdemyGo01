1f8b200dde26e398b7ff721e683e93748b9aec39
Antoine Ferron <antoine.ferron6@gmail.com>
2024-09-15 15:20:29 +0200

    Defer

--- main.go
+++ main.go
@@ -1,30 +1,79 @@
 package main
 
+//##########################################
+//##################ERRORS##################
+//##########################################
+//
+//import (
+//	"fmt"
+//	"os"
+//)
+//
+//func readFile(filename string) (string, error) {
+//	data, err := os.ReadFile(filename)
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	if len(data) == 0 {
+//		//return "", errors.New("empty content") // erreur simple
+//		return "", fmt.Errorf("empty content (filename=\"%v\")", filename) // erreur avec formatage pour ajouter des informations supplémentaires tel que le nom du fichier
+//	}
+//
+//	return string(data), nil
+//}
+//
+//func main() {
+//	dat, err := readFile("./test.txt")
+//	if err != nil {
+//		fmt.Printf("Error while reading file: %v\n", err)
+//		return
+//	}
+//	fmt.Println("Content:")
+//	fmt.Println(dat)
+//}
+
+// ##########################################
+// ##################DEFER###################
+// ##########################################
+// permet de reporter l'exécution d'une fonction jusqu'à la fin de la fonction courante
+
 import (
 	"fmt"
-	"os"
 )
 
-func readFile(filename string) (string, error) {
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		return "", err
-	}
+func start() {
+	fmt.Println("Start")
+}
 
-	if len(data) == 0 {
-		//return "", errors.New("empty content") // erreur simple
-		return "", fmt.Errorf("empty content (filename=\"%v\")", filename) // erreur avec formatage pour ajouter des informations supplémentaires tel que le nom du fichier
-	}
-
-	return string(data), nil
+func finish() {
+	fmt.Println("Finish")
 }
 
 func main() {
-	dat, err := readFile("./test.txt")
-	if err != nil {
-		fmt.Printf("Error while reading file: %v\n", err)
-		return
+	//start()
+	//defer finish()
+	//
+	//names := []string{"Bob", "Alice", "Bobette", "John"}
+	//for _, n := range names {
+	//	fmt.Printf("Hello %v\n", n)
+	//  fmt.Printf("Goodbye %v\n", n)
+	//}
+
+	start()
+	defer finish() // defer fonctionne en LIFO (Last In First Out)
+	// ordre d'ajout dans la pile du defer
+	// 1. finish()          5
+	// 2. Goodbye Bob     ^ 4
+	// 3. Goodbye Alice   ^ 3
+	// 4. Goodbye Bobette ^ 2
+	// 5. Goodbye John    ^ 1
+	//                    ordre d'éxécution de la pile du defer
+
+	names := []string{"Bob", "Alice", "Bobette", "John"}
+	for _, n := range names {
+		fmt.Printf("Hello %v\n", n)
+		defer fmt.Printf("Goodbye %v\n", n) // ici on ajoute chaque Printf dans la pile du defer de la fonction courante
 	}
-	fmt.Println("Content:")
-	fmt.Println(dat)
+
 }
